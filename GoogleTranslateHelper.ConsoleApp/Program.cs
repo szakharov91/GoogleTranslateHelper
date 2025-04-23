58c74b894d60df3b0c7941a6dd52b0d50f2c8781
Zakharov Svyatoslav <kraldraav@gmail.com>
2025-04-23 23:47:08 +0300

    Redesign folder structure, add console application

---
+++ GoogleTranslateHelper.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleTranslateHelper.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GtCore.Get().Translate("Hello, World!", "ru"));
+
+            Console.ReadLine();
+        }
+    }
+}
