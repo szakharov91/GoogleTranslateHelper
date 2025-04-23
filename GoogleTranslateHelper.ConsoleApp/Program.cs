e174e517d80551b572781b8be4636c9a5b66437c
Zakharov Svyatoslav <kraldraav@gmail.com>
2025-04-24 00:35:50 +0300

    Add one more test line in console app

--- GoogleTranslateHelper.ConsoleApp/Program.cs
+++ GoogleTranslateHelper.ConsoleApp/Program.cs
@@ -12,6 +12,10 @@
 
             Console.WriteLine(GtCore.Get().SetContent("Hello, World!").To("hr"));
 
+            Console.WriteLine(GtCore.Get().Translate("На краю дороги стоял дуб. " +
+                "Вероятно, в десять раз старше берёз, составлявших лес, " +
+                "он был в десять раз толще и в два раза выше каждой берёзы.", "fr"));
+
             Console.ReadLine();
         }
     }
